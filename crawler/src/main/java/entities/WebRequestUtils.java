01a918256122d7352e6782bdb57b0175b26b0bb0
dougdasilva <douglas_wfs@hotmail.com>
2023-08-14 22:35:38 -0300

    create request's http

---
+++ crawler/src/main/java/entities/WebRequestUtils.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class WebRequestUtils {
+
+    protected static HttpClient client;
+
+    protected static HttpResponse<String> response;
+
+    protected static HttpRequest request;
+
+    public static HttpResponse<String> get() {
+        try {
+            client = HttpClient.newBuilder().build();
+            request = HttpRequest
+                    .newBuilder()
+                    .GET()
+                    .uri(URI.create("https://http.cat/"))
+                    .build();
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed request", e);
+        }
+    }
+
+    public static HttpResponse<String> post(String payload) {
+        try {
+            client = HttpClient.newBuilder().build();
+            request = HttpRequest
+                    .newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(payload))
+                    .uri(URI.create("https://http.cat/"))
+                    .build();
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed request", e);
+        }
+    }
+}
